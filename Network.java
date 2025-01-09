84e0c949c2ca406d4266a77e9f1318a563359d2e
nicole yerushalmi <nicole.yerushalmi@post.ac.il>
2025-01-09 23:32:08 +0200

    last try

--- Network.java
+++ Network.java
@@ -155,12 +155,12 @@ public class Network {
     public String toString() {
        //// Replace the following statement with your code2
        //System.out.println("network: ");
-       String new_str ="Network:";
+       String new_str ="Network:" + "\n";
        for(int i = 0; i< userCount;i++){
-        new_str = "\n"+new_str + " "+ "\n"+this.users[i].getName() + " -> ";
+        new_str = new_str + " "+ "\n"+this.users[i].getName() + " -> ";
         for(int j = 0; j<this.users[i].getfFollows().length; j++){
             if(this.users[i].getfFollows()[j] != null){
-            new_str ="\n" + new_str + "\n" + this.users[i].getfFollows()[j];}
+            new_str = new_str + "\n" + this.users[i].getfFollows()[j];}
            
         }
        }
