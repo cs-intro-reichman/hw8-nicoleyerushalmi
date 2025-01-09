3832edeb546f4ea6fecb54d10403ad766a8093ef
nicole yerushalmi <nicole.yerushalmi@post.ac.il>
2025-01-09 23:02:55 +0200

    last try

--- Network.java
+++ Network.java
@@ -152,7 +152,7 @@ public class Network {
        System.out.println("network: ");
        String new_str ="Network:\n";
        for(int i = 0; i< userCount;i++){
-        new_str = new_str + this.users[i].getName() + "-> ";
+        new_str = new_str + this.users[i].getName() + " -> ";
         for(int j = 0; j<this.users[i].getfFollows().length; j++){
             if(this.users[i].getfFollows()[j] != null){
             new_str = new_str + this.users[i].getfFollows()[j] + "\n";}
